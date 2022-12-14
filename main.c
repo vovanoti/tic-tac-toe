01fd87319f42a55c7d2a07ef1f29d5279e3ce944
vovanoti <v_smirnovch@mail.ru>
2022-12-15 01:13:58 +0300

    fixed input characters bug

--- main.c
+++ main.c
@@ -20,20 +20,40 @@ void twoplayergame();
 void singleplayergame();
 void resetfield(int arr[], int count);
 int modeselectf(int a);
+int userinputf();
 
 int main(){
 	mainmenu();
-	int modeselect = 10;
 	while(1){
 		printf("================================================================\n");
 		printf("Please choose the number and press Enter to select a game mode\n(1 - single player, 2 - two players, 3 - help, 0 - exit):");
-		scanf("%10i",&modeselect);
-		int exitmode = modeselectf(modeselect);
-		//printf("DEBUG: EXITMODE IS %i\n",exitmode);
+		int exitmode = modeselectf(userinputf());
 		if (exitmode == 1)
 			break;
 	}
 	return 0;
+}
+
+int modeselectf(int a){
+	switch(a){
+		case 1:
+		system(clear);
+		printf("Single mode is under development...\n");
+		resetfield(field, NFIELD);
+		//singleplayergame();
+		break;
+		case 2:
+		resetfield(field, NFIELD);
+		twoplayergame();
+		break;
+		case 3:
+		helpmenu();
+		break;
+		case 0:
+		printf("Bye bye!\n");
+		return 1;
+		break;
+	}
 }
 
 void singleplayergame(){
@@ -43,7 +63,7 @@ void singleplayergame(){
 			table(field);
 			while (p1 > 9 || p1 < 1 || field[p1-1] != 0){
 				printf("Player 1 (X): Please enter the number 1-9\n");
-				scanf("%10i", &p1);
+				p1 = userinputf();
 			}
 			field[p1-1] = 1;
 			p1 = 10;
@@ -53,8 +73,10 @@ void singleplayergame(){
 			while (cpu > 9 || cpu < 1 || field[cpu-1] != 0){ 
 				printf("Player 2 (O): Please enter the number 1-9\n");
 				//here should be the cpu
-				cpu = 1;
-				//scanf("%10i", &p2);
+				if (field[4] == 1)
+					cpu = 1;
+				else
+					cpu = 5;
 			}
 			field[cpu-1] = 2;
 			cpu = 10;
@@ -71,7 +93,7 @@ void twoplayergame(){
 		table(field);
 		while (p1 > 9 || p1 < 1 || field[p1-1] != 0){
 			printf("Player 1 (X): Please enter the number 1-9\n");
-			scanf("%10i", &p1);
+			p1 = userinputf();
 		}
 		field[p1-1] = 1;
 		p1 = 10;
@@ -80,7 +102,7 @@ void twoplayergame(){
 		table(field);
 		while (p2 > 9 || p2 < 1 || field[p2-1] != 0){ 
 			printf("Player 2 (O): Please enter the number 1-9\n");
-			scanf("%10i", &p2);
+			p2 = userinputf();
 		}
 		field[p2-1] = 2;
 		p2 = 10;
@@ -97,28 +119,6 @@ void twoplayergame(){
 		break;
 	case 3:
 		printf("======Oops, it's a tie! Looks like we have no winner today======\n");
-		break;
-	}
-}
-
-int modeselectf(int a){
-	switch(a){
-		case 1:
-		system(clear);
-		printf("Single mode is under development...\n");
-		resetfield(field, NFIELD);
-		singleplayergame();
-		break;
-		case 2:
-		resetfield(field, NFIELD);
-		twoplayergame();
-		break;
-		case 3:
-		helpmenu();
-		break;
-		case 0:
-		printf("Bye bye!\n");
-		return 1;
 		break;
 	}
 }
@@ -200,6 +200,17 @@ void table(int arr[]){
     printf("\t\t\t# %c # # %c # # %c #\n",point[0],point[1],point[2]);
     printf("\t\t\t##### ##### #####\n");
 	printf("================================================================\n");
+}
+
+int userinputf(){
+	char userinput[40];
+	scanf("%39s",userinput);
+	char ch = userinput[0];
+	for(int i=48; i<58; i++){
+		if (ch == i)
+			return ch -'0';
+	}
+	return 10;
 }
 
 //debugging function
